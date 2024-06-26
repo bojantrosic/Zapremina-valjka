893561f23c92b091578bb9bcd319fcc59974a5f5
bojantrosic <151999309+bojantrosic@users.noreply.github.com>
2024-06-26 16:54:10 +0200

    Add files via upload

---
+++ Zapremina valjka/script2.js
@@ -0,0 +1,6 @@
+let poluprecnik = prompt("Molim unesite radius:");
+let visina = prompt("Molim  unesite visinu:");
+let pi = 3.14;
+
+let zapremina_valjka = parseInt(poluprecnik) * parseInt(poluprecnik) * parseInt(visina) * pi;
+alert("Zapremina valjka je: " + zapremina_valjka);
